4b20233784f4b3cd105bcda918179f18015eab27
Pongsakorn Amornmaturapod <pongsakorn.amorn@outlook.co.th>
2023-08-27 01:14:16 +0000

    [token] reset.

--- somchai-bn/Configuration/AwsConfiguration.cs
+++ somchai-bn/Configuration/AwsConfiguration.cs
@@ -2,8 +2,8 @@
 {
     public static class AwsConfiguration
     {
-        public static string accessKey { get; } = "ASIA5YOHYLTUGP6V6KE2";
-        public static string secretKey { get; } = "9GRYl4myqDRHRkC40BRXKSKalhZz/U2fTA6PScG1";
+        public static string accessKey { get; } = "ASIA5YOHYLTUL65SREMQ";
+        public static string secretKey { get; } = "2CbJRcp8zoBeIms99VtX90qfnzwBNGk6zoXYl2Su";
         public static string token { get; } = "IQoJb3JpZ2luX2VjEKH//////////wEaCXVzLWVhc3QtMSJHMEUCIQDwpxfnuMWzL0t1l0qYmnZkUAMhjVM1Cxsk0Oh5ShcRKAIgQJP+mnN1lHZWLeuaJgOSOnDRkV+V4SlYANJ88O0nVdsqlwIIahABGgw5NDU4NDg2MDU5MjgiDDca/sEECIolAOXE1Cr0Af9RgYJ7auuoud/e596ErllRNnrk75fPOiGfAaKc43Yz8fmRfWQeygcQWmVzM2Q+ydCqfDuZLtew0KwMySpB0qQlUMY6YOBIf4ZX5lpiimcikOOyTieyQ4YPn8vfChApg4m4J9+F58027OSE1wlrzOE/AT6KZH85KvqY8WNoZmM+W8+EDbdcft/VFlkA94oYYZJ8/dydD1v3wyNN4pdNxtk5g31lWuqw3H8DGoJc39ruMiQ9W2iJiLqtBJXS0h1Av4KujcQnLjnnozBxXP5gaqF8xkNCSYyUl5xxn/OIP5lrk9BaWTA74g22jLnxnQw28+0dC84wyLuqpwY6nQFkK4xKxz8/cYuf3RKAb+w1srh/rLUAVyhYyOUGpgtcfFKuffEnVWXX1iXdL5o8YkA6zFGMSeAiCQR79b5fhkIjMH/VJXSSMQ5doaigJoSIhs3CNOBokFmss+jOovqqPT+0ETjt5gHXAybFovg6MRL8KJ/5NXaawLmsxDE43hp246libsy+hroNB1UtOkWkdwcvHiwpZoPS+u3Ho+ir";
 
     }
